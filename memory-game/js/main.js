c821570178803b5441051cb2762a7f7300cf4915
Maleeha <malehahasan@gmail.com>
2019-10-14 02:13:19 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay=[];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push(cardOne);
+console.log("USER flipped " + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo);
+console.log("USER flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2)
+{
+	if(cardsInPlay[0] === cardsInPlay[1])
+		{alert("you found a match!");}
+	else
+		{alert("Sorry try again");}
+}
